d104685958e32c1faa91c463a3527e62313ffa15
WW <13792916787@163,com>
2021-03-20 15:15:13 +0800

    ?

--- app/src/main/java/com/ww/flower/ui/activities/ui/home/HomeFragment.kt
+++ app/src/main/java/com/ww/flower/ui/activities/ui/home/HomeFragment.kt
@@ -4,12 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.SavedStateViewModelFactory
 import androidx.lifecycle.ViewModelProvider
-import com.ww.flower.R
 import com.ww.flower.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
